ef1e07bfa2f5d3c16e8b2df3290e0228549b2272
nike-getto <savastanisic7@gmail.com>
2023-08-31 15:07:14 +0200

    changed file to tmp :)

--- week2/week2.sh
+++ week2/week2.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
 echo "Creating new file called 'week_2_file'" && \
-touch /home/bobos/Downloads/bloxico/week1/temp/week_2_file
+touch //tmp/week_2_file
 
 echo "Changing file permissions to 666" && \
-sudo chmod 666 /home/bobos/Downloads/bloxico/week1/temp/week_2_file
+sudo chmod 666 //tmp/week_2_file
 
 echo "Changing file owner to root" && \
-sudo chown root /home/bobos/Downloads/bloxico/week1/temp/week_2_file
+sudo chown root //tmp/week_2_file
 
 echo "Printing file's permissions and ownership" && \
-ls -l /home/bobos/Downloads/bloxico/week1/temp/week_2_file
+ls -l //tmp/week_2_file
 
 echo "Printing size of current directory" && \
 du -sh
