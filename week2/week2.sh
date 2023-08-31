e040e01487f75ea8582b86fa91f77d06958c0226
nike-getto <savastanisic7@gmail.com>
2023-08-31 14:41:49 +0200

    added full file path

--- week2/week2.sh
+++ week2/week2.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
 echo "Creating new file called 'week_2_file'" && \
-touch ../week1/temp/week_2_file
+touch /home/bobos/Downloads/bloxico/week1/temp/week_2_file
 
 echo "Changing file permissions to 666" && \
-sudo chmod 666 ../week1/temp/week_2_file
+sudo chmod 666 /home/bobos/Downloads/bloxico/week1/temp/week_2_file
 
 echo "Changing file owner to root" && \
-sudo chown root ../week1/temp/week_2_file
+sudo chown root /home/bobos/Downloads/bloxico/week1/temp/week_2_file
 
 echo "Printing file's permissions and ownership" && \
-ls -l ../week1/temp/week_2_file
+ls -l /home/bobos/Downloads/bloxico/week1/temp/week_2_file
 
 echo "Printing size of current directory" && \
 du -sh
